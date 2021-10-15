b75b0de3e5b017ad23b64973a726cc6a95212517
Keos <turbogara1@gmail.com>
2021-10-15 16:05:58 +0900

    [Edit]로그 색깔 변경

--- logger.js
+++ logger.js
@@ -4,10 +4,9 @@ module.exports = {
     },
     log: console.log,
     warn: function (...params) {
-        //업데이트 확인용
         console.log("\u001b[31m⚠\u001b[0m", ...params)
     },
     error: function (...params) {
-        console.log("❌", ...params)
+        console.log("❌" + "\u001b[31m", ...params, "\u001b[0m")
     },
 }
